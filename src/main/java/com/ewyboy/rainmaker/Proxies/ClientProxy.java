e10ae0df7d3120d61ff02be00732ba10c377e5b2
EwyBoy <EwyBoy@gmail.com>
2015-04-07 10:18:26 +0200

    Update ClientProxy.java

--- src/main/java/com/ewyboy/rainmaker/Proxies/ClientProxy.java
+++ src/main/java/com/ewyboy/rainmaker/Proxies/ClientProxy.java
@@ -4,9 +4,10 @@ import com.ewyboy.rainmaker.RainMakerRender;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy {
-
+    
     public int rainmakerRenderID;
 
+    //Registering the rendering of the block to the clientsided proxy. Since rendering is done only on the client.
     public void initRenders() {
         RainMakerRender rainMakerRender = new RainMakerRender();
         RenderingRegistry.registerBlockHandler(rainMakerRender);
